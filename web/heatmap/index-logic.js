21ca19ad01b2889c3d5c0cd17e1a3406806e7617
JP Meijers <gitkraken@jpmeijers.com>
2019-12-26 15:50:13 +0200

    Make background layer choosable via get parameter - on all new bootstrap maps

--- web/heatmap/index-logic.js
+++ web/heatmap/index-logic.js
@@ -124,7 +124,7 @@ function addBackgroundLayers() {
     maxZoom: 20,
     ext: 'png',
     fadeAnimation: false
-  }).addTo(map);
+  });
 
 
   var OpenStreetMap_Mapnik_Grayscale = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -147,6 +147,24 @@ function addBackgroundLayers() {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     fadeAnimation: false
   });
+
+  switch (findGetParameter("layer")) {
+    case "mapnik":
+      OpenStreetMap_Mapnik.addTo(map);
+      break;
+    case "mapnik_grayscale":
+      OpenStreetMap_Mapnik_Grayscale.addTo(map);
+      break;
+    case "terrain":
+      Esri_WorldShadedRelief.addTo(map);
+      break;
+    case "satellite":
+      Esri_WorldImagery.addTo(map);
+      break;
+    default:
+      // use default layer
+      Stamen_TonerLite.addTo(map);
+  }
 
 
   L.control.layers({
