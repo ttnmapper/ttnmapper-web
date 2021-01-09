8762f45332b931e8eb6c6d5c253a9c8443f71981
JP Meijers <git@jpmeijers.com>
2021-01-09 09:32:12 +0000

    Replace favicons by new ones generated using https://realfavicongenerator.net/

--- web/head.php
+++ web/head.php
@@ -55,22 +55,16 @@ if(isset($settings['analytics']['site_id'])) {
   <title><?php echo $siteName; ?></title>
 
   <!--favicons-->
-  <link rel="apple-touch-icon" sizes="57x57" href="/<?php echo $favicons; ?>/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/<?php echo $favicons; ?>/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/<?php echo $favicons; ?>/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/<?php echo $favicons; ?>/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/<?php echo $favicons; ?>/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/<?php echo $favicons; ?>/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/<?php echo $favicons; ?>/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/<?php echo $favicons; ?>/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/<?php echo $favicons; ?>/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="/<?php echo $favicons; ?>/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/<?php echo $favicons; ?>/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/<?php echo $favicons; ?>/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/<?php echo $favicons; ?>/favicon-16x16.png">
-  <link rel="manifest" href="/<?php echo $favicons; ?>/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="/<?php echo $favicons; ?>/ms-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/<?php echo $favicons; ?>/apple-touch-icon.png?v=20210109">
+  <link rel="icon" type="image/png" sizes="32x32" href="/<?php echo $favicons; ?>/favicon-32x32.png?v=20210109">
+  <link rel="icon" type="image/png" sizes="16x16" href="/<?php echo $favicons; ?>/favicon-16x16.png?v=20210109">
+  <link rel="manifest" href="/<?php echo $favicons; ?>/site.webmanifest?v=20210109">
+  <link rel="mask-icon" href="/<?php echo $favicons; ?>/safari-pinned-tab.svg?v=20210109" color="#5bbad5">
+  <link rel="shortcut icon" href="/<?php echo $favicons; ?>/favicon.ico?v=20210109">
+  <meta name="apple-mobile-web-app-title" content="TTN Mapper">
+  <meta name="application-name" content="TTN Mapper">
+  <meta name="msapplication-TileColor" content="#2d89ef">
+  <meta name="msapplication-config" content="/<?php echo $favicons; ?>/browserconfig.xml?v=20210109">
   <meta name="theme-color" content="#ffffff">
 
 
