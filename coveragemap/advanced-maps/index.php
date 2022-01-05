3ce9f187060154c0c0db349d6dafc59f51706b56
JP Meijers <git@jpmeijers.com>
2022-01-05 14:39:51 +0200

    Consistent heatmap naming

--- coveragemap/advanced-maps/index.php
+++ coveragemap/advanced-maps/index.php
@@ -20,7 +20,7 @@
     <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/heatmap/">Heatmap</a>
+                <a class="nav-link" href="/heatmap/">Helium Heatmap</a>
             </li>
             <li class="nav-item active">
                 <a class="nav-link" href="/advanced-maps/">Advanced maps</a>
